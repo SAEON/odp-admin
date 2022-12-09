e8bf90af1cc4735eed692728e8ae893499f3931f
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-12-09 10:44:07 +0200

    Move common app init code to core

--- odp/ui/admin/__init__.py
+++ odp/ui/admin/__init__.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 
 from flask import Flask
-from jinja2 import ChoiceLoader, FileSystemLoader
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 from odp.config import config
 from odp.const import ODPScope
@@ -18,21 +16,12 @@ def create_app():
     app = Flask(__name__)
     app.config.update(
         SECRET_KEY=config.ODP.UI.ADMIN.FLASK_KEY,
-        SESSION_COOKIE_SECURE=True,
-        SESSION_COOKIE_SAMESITE='Lax',
         UI_CLIENT_ID=config.ODP.UI.ADMIN.CLIENT_ID,
         UI_CLIENT_SECRET=config.ODP.UI.ADMIN.CLIENT_SECRET,
         UI_CLIENT_SCOPE=[HydraScope.OPENID, HydraScope.OFFLINE_ACCESS] + [s.value for s in ODPScope],
     )
 
-    ui_dir = Path(__file__).parent.parent
-    app.jinja_loader = ChoiceLoader([
-        FileSystemLoader(ui_dir / 'admin' / 'templates'),
-        FileSystemLoader(base.TEMPLATE_DIR),
-    ])
-    app.static_folder = base.STATIC_DIR
-
-    base.init_app(app, user_api=True)
+    base.init_app(app, user_api=True, template_dir=Path(__file__).parent / 'templates')
     views.init_app(app)
 
     return app
